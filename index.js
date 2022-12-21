1843c25e3981d68c1a9483d126b276956de46e47
Felix <felixlam0316@gmail.com>
2022-12-21 08:40:34 -0500

    - Added NAICS and Industry labels

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ import logbuffer from "console-buffer";
 import JSONStream from "JSONStream";
 // import pkg from 'csv-parse';
 import * as csv from 'csv-parse';
+import pkg from "csv-parser";
 import * as path from 'path';
 
 const program = new Command();
@@ -21,9 +22,11 @@ program
 
 program
 	.option("-i, --input <path>", "The input source file")
+    .option("-naics, --naicsList <path>", "Get the list of NAICS data")
     .option("-d, --download <params>", "The download params (all/#)")
     .option("-o, --output <path>", "The output file path")
     .option("-c, --convert <params>", "The convert params (all/#)")
+    .option("-p, --package <params>", "The package params (all/#)")
 
 program.parse(process.argv);
 
@@ -48,7 +51,65 @@ async function Init() {
     var results = [];
     //sloppy quick implementation
 
-var count = 0;
+    var count = 0;
+
+    if(options.package)
+    {
+        await PackageAllCSVIntoOne();
+    } else {
+        await ParseConvert();
+    }
+    
+}
+
+
+async function WriteToXML(json, outputPath, name) {
+	if (!fs.existsSync(outputPath)) {
+		fs.mkdirSync(outputPath, {
+			recursive: true,
+		});
+	}
+
+    // console.log(json)
+	var destination = name ? outputPath + "/" + name : outputPath + "/out.csv";
+
+	var xmlData = json.map((el) => {
+        return Object.values(el).join(",");
+    }).join("\n");
+
+	fs.writeFile(destination, xmlData, "utf8", function (err) {
+		if (err) {
+			console.log("An error occured while writing JSON Object to File.");
+			console.log(err);
+			return;
+		}
+        
+		console.log("File has been saved at: " + destination);
+	});
+}
+
+async function ParseConvert() {
+    var naicsListCode = [];
+    var naicsListTitle = [];
+    var results = [];
+    var count = 0;
+    var first = true;
+
+    if(options.naicsList)
+    {
+        fs.createReadStream(options.naicsList)
+        .pipe(pkg())
+        .on('data', (data) => {
+            naicsListCode.push(data.Code);
+            naicsListTitle.push(data.Title);
+        })
+        .on('end', () => {
+            console.log("Completed reading Naics List");
+            req.resume();
+        });
+    }
+    
+
     var req = fs.createReadStream(options.input)
     .pipe(csv.parse())
     .on('data', (data) => {
@@ -66,9 +127,22 @@ var count = 0;
             "FranchiseName": data[16],
             "JobsReported": data[32],
             "NAICSCode": data[33],
+            "Industry": "Industry",
             "BusinessType": data[43],
             "ForgivenessAmount": data[51],
             "ForgivenessDate": data[52]
+        }
+        
+        if(options.naicsList && naicsListCode.indexOf(template.NAICSCode) != -1)
+        {
+            if(first)
+            {
+                first=false;
+            } else {
+                template.Industry = naicsListTitle[naicsListCode.indexOf(template.NAICSCode)];
+            }
+        } else {
+            template.Industry = "";
         }
         
         Object.keys(template).forEach((el) => { 
@@ -101,30 +175,92 @@ var count = 0;
         logbuffer.flush();
         await WriteToXML(results, options.output, path.basename(options.input));
     });
+    
+    if(options.naicsList)
+        req.pause();
 }
 
-
-async function WriteToXML(json, outputPath, name) {
-	if (!fs.existsSync(outputPath)) {
+async function PackageAllCSVIntoOne()
+{
+    var outputPath = options.output;
+    var name;
+    var destination = name ? outputPath + "/" + name : outputPath + "/out.csv";
+    var filesList = [];
+    var fileCount = 0;
+    var count = 0;
+    
+    if (!fs.existsSync(outputPath)) {
 		fs.mkdirSync(outputPath, {
 			recursive: true,
 		});
 	}
 
-    // console.log(json)
-	var destination = name ? outputPath + "/" + name : outputPath + "/out.csv";
+    var outputStream = fs.createWriteStream(destination);
 
-	var xmlData = json.map((el) => {
-        return Object.values(el).join(",");
-    }).join("\n");
+    outputStream.on('error', function (err) {
+        console.log(err);
+    });
 
-	fs.writeFile(destination, xmlData, "utf8", function (err) {
-		if (err) {
-			console.log("An error occured while writing JSON Object to File.");
-			console.log(err);
-			return;
-		}
-        
-		console.log("File has been saved at: " + destination);
-	});
+    fs.readdirSync(options.input).forEach(file => filesList.push(file))
+
+    console.log(filesList)
+
+    if(filesList.length == 0)
+    {
+        console.log("No files found in the input folder");
+        return;
+    }
+    
+    var inputStream = fs.createReadStream(options.input)
+    inputStream
+        .on('error', function (err) {
+            console.log(err);
+        })
+
+    
+    var req = fs.createReadStream(options.input + "/" + filesList[0])
+    req
+        .pipe(csv.parse())
+        .on('data', (chunk) => { 
+            console.log(count++);
+            console.log(chunk.length)
+            var xmlData = chunk.map((el) => {
+                return Object.values(el);
+            }).join("\n");
+            outputStream.write(xmlData);
+        })
+        .on("end", () => {
+            console.log("Done");
+            fileCount++;
+            CreateNewPipe(filecount)
+        })
+        .on("error", (err) => {
+            console.log(err);
+        })
+
+    function CreateNewPipe(fileCount)
+    {
+        if(fileCount < filesList.length)
+        {
+            return;
+        }
+
+        var req = fs.createReadStream(options.input + "/" + filesList[fileCount])
+        req
+            .pipe(csv.parse())
+            .on('data', (chunk) => { 
+                var xmlData = chunk.map((el) => {
+                    return Object.values(el).join(",");
+                }).join("\n");
+                outputStream.write(xmlData);
+            })
+            .on("end", () => {
+                console.log("Done");
+                fileCount++;
+                CreateNewPipe(fileCount);
+            })
+            .on("error", (err) => {
+                console.log(err);
+            })
+    }
 }
